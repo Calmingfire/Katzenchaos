1d3f6179d6e843e8ec08660099fe8f53c9bf102d
CalmingFire <timclemens123@gmail.com>
2025-07-19 11:58:50 +0200

    Bouncefuction nun mit einstellbarer Geschwindigkeit, Mehr earvape

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const catSoundArray = [new Audio("catsound1.mp3"), new Audio("catsound2.mp3") ,new Audio("catsound3.mp3") ,new Audio("catsound4.mp3") ,new Audio("catsound5.mp3"),new Audio("catsound6.mp3")]
-
 
 //gültig für anfangObenOderUnten: "oben", "unten", anfangRechtsOderLinks: "rechts", "links"
-async function tvLogoBounce(htmlElement, anfangObenOderUnten, anfangRechtsOderLinks ) //lässt ein html-element an den rändern seines Parends bouncen
+//für geschwidigkeit: kleinere zahl ist gleich schneller
+//SchnellerJaoderNein: boolean
+async function tvLogoBounce(htmlElement, anfangObenOderUnten, anfangRechtsOderLinks, anfangsGeschwindigkeit, soundarray,) //lässt ein html-element an den rändern seines Parends bouncen
 {
-    console.log(anfangObenOderUnten)
-    console.log(anfangRechtsOderLinks)
+    let geschwidigkeit = anfangsGeschwindigkeit;
     let richtungHorizontal = "rechts" //speichert in welche richtung das Element sich bewegt
     let richtungVertikal = "unten";
     htmlElement.style.position = "absolute";
@@ -13,15 +12,16 @@ async function tvLogoBounce(htmlElement, anfangObenOderUnten, anfangRechtsOderLi
     htmlElement.style.right = "";
     let iteration = 0;
     //Anfangspositionierung
-    if(anfangObenOderUnten === "unten") {htmlElement.style.top = parseInt(getComputedStyle(htmlElement.parentElement).height) -
-         parseInt(getComputedStyle(htmlElement).height) 
-         + "px" ; }
-    else{htmlElement.style.top = "0px";}
-    if(anfangRechtsOderLinks === "rechts") {htmlElement.style.left =  parseInt(getComputedStyle(htmlElement.parentElement).width) -parseInt(getComputedStyle(htmlElement).width) + "px"; }
-    else {htmlElement.style.left = "0px";}
+    if (anfangObenOderUnten === "unten") {
+        htmlElement.style.top = parseInt(getComputedStyle(htmlElement.parentElement).height) -
+            parseInt(getComputedStyle(htmlElement).height)
+            + "px";
+    }
+    else { htmlElement.style.top = "0px"; }
+    if (anfangRechtsOderLinks === "rechts") { htmlElement.style.left = parseInt(getComputedStyle(htmlElement.parentElement).width) - parseInt(getComputedStyle(htmlElement).width) + "px"; }
+    else { htmlElement.style.left = "0px"; }
 
-    const intervalID = setInterval( () =>
-    {  
+    const intervalID = setInterval(() => {
         let neachsterLeftWert;
         let naechsterTopWert;
         if (richtungHorizontal === "rechts") //berechung für style.left
@@ -29,7 +29,10 @@ async function tvLogoBounce(htmlElement, anfangObenOderUnten, anfangRechtsOderLi
             if (parseInt(getComputedStyle(htmlElement).left) + parseInt(getComputedStyle(htmlElement).width) >= parseInt(getComputedStyle(htmlElement.parentElement).width)) { //Richtungswächsel
                 neachsterLeftWert = parseInt(getComputedStyle(htmlElement).left) - 1 + "px";
                 richtungHorizontal = "links";
-                 if(iteration !== 0) {playRandomCatSound();}
+                if (iteration !== 0) {
+                    playRandomCatSound(soundarray);
+                     }
+                
             }
             else //normal in die Richtung weiter
             {
@@ -41,7 +44,11 @@ async function tvLogoBounce(htmlElement, anfangObenOderUnten, anfangRechtsOderLi
             if (parseInt(getComputedStyle(htmlElement).left) <= 0) { //Richtungswächsel
                 neachsterLeftWert = parseInt(getComputedStyle(htmlElement).left) + 1 + "px";
                 richtungHorizontal = "rechts";
-                if(iteration !== 0) {playRandomCatSound();}
+                if (iteration !== 0) 
+                    {
+                    playRandomCatSound(soundarray);
+                   
+                }
             }
             else //normal in die Richtung weiter
             {
@@ -53,11 +60,14 @@ async function tvLogoBounce(htmlElement, anfangObenOderUnten, anfangRechtsOderLi
         {
 
             //Richtungswächsel
-            if (parseInt(getComputedStyle(htmlElement).top) + parseInt(getComputedStyle(htmlElement).height) >= parseInt(getComputedStyle(htmlElement.parentElement).height)) 
-                { 
+            if (parseInt(getComputedStyle(htmlElement).top) + parseInt(getComputedStyle(htmlElement).height) >= parseInt(getComputedStyle(htmlElement.parentElement).height)) {
                 naechsterTopWert = parseInt(getComputedStyle(htmlElement).top) - 1 + "px";
                 richtungVertikal = "oben";
-                if(iteration !== 0) {playRandomCatSound();}
+                if (iteration !== 0) 
+                    {
+                    playRandomCatSound(soundarray);
+                    
+                }
             }
             else //normal in die Richtung weiter
             {
@@ -69,7 +79,10 @@ async function tvLogoBounce(htmlElement, anfangObenOderUnten, anfangRechtsOderLi
             if (parseInt(getComputedStyle(htmlElement).top) <= 0) { //Richtungswechsel
                 naechsterTopWert = parseInt(getComputedStyle(htmlElement).top) + 1 + "px";
                 richtungVertikal = "unten";
-                if(iteration !== 0) {playRandomCatSound();}
+                if (iteration !== 0) {
+                    playRandomCatSound(soundarray);
+                    
+                }
             }
             else //normal in die Richtung weiter
             {
@@ -81,24 +94,34 @@ async function tvLogoBounce(htmlElement, anfangObenOderUnten, anfangRechtsOderLi
         htmlElement.style.top = naechsterTopWert;
         htmlElement.style.left = neachsterLeftWert;
         iteration++;
-    } 
-    , 5); //ende des intervalls
- setTimeout( () => clearInterval(intervalID), 1000000 );
+        
+    }
+        , geschwidigkeit); //ende des intervalls
+    setTimeout(() => clearInterval(intervalID), 1000000);
 }
-function playRandomCatSound()
-{
-    const soundPicker =   getRndInteger(0, catSoundArray.length - 1);
-    catSoundArray[soundPicker].play();
+function playRandomCatSound(soundarray) {
+    const soundPicker = getRndInteger(0, soundarray.length - 1);
+    soundarray[soundPicker].play();
 }
 function getRndInteger(min, max) { //  gibt eine zahl zwischen min und max, beide mit engeschlossen.
-  return Math.floor(Math.random() * (max - min + 1) ) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function katzenChaos() 
-{
-    tvLogoBounce(document.getElementById("katzeText"),"oben","links");
-    tvLogoBounce(document.getElementById("katzeBild1"), "unten", "rechts");
-    tvLogoBounce(document.getElementById("katzeBild2"), "oben", "rechts");
-    tvLogoBounce(document.getElementById("katzeBild3"), "oben", "links");
-    tvLogoBounce(document.getElementById("katzeBild4"), "unten", "links")
+function katzenChaos() {
+
+    const catSoundArray1 = [new Audio("catsound1.mp3"), new Audio("catsound2.mp3"), new Audio("catsound3.mp3"), new Audio("catsound4.mp3"), new Audio("catsound5.mp3"), new Audio("catsound6.mp3")];
+    const catSoundArray2 = [new Audio("catsound1.mp3"), new Audio("catsound2.mp3"), new Audio("catsound3.mp3"), new Audio("catsound4.mp3"), new Audio("catsound5.mp3"), new Audio("catsound6.mp3")];
+    const catSoundArray3 = [new Audio("catsound1.mp3"), new Audio("catsound2.mp3"), new Audio("catsound3.mp3"), new Audio("catsound4.mp3"), new Audio("catsound5.mp3"), new Audio("catsound6.mp3")];
+    const catSoundArray4 = [new Audio("catsound1.mp3"), new Audio("catsound2.mp3"), new Audio("catsound3.mp3"), new Audio("catsound4.mp3"), new Audio("catsound5.mp3"), new Audio("catsound6.mp3")];
+    const catSoundArray5 = [new Audio("catsound1.mp3"), new Audio("catsound2.mp3"), new Audio("catsound3.mp3"), new Audio("catsound4.mp3"), new Audio("catsound5.mp3"), new Audio("catsound6.mp3")];
+
+
+    tvLogoBounce(document.getElementById("katzeText"), "oben", "links", 1 , catSoundArray1 );
+    tvLogoBounce(document.getElementById("katzeBild1"), "unten", "rechts", 1, catSoundArray2 );
+    tvLogoBounce(document.getElementById("katzeBild2"), "oben", "rechts", 1,catSoundArray3);
+    tvLogoBounce(document.getElementById("katzeBild3"), "oben", "links", 1,catSoundArray4) ;
+    tvLogoBounce(document.getElementById("katzeBild4"), "unten", "links",1 ,catSoundArray5);
 }
+let test = 3;
+test /= 2;
+console.log(test)
